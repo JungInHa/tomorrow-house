e2a0e4f1f1a9e4c83ab87b6cb52293a79c96b397
JungInHa <jungin.dev@gmail.com>
2023-09-19 17:24:11 +0900

    ✨ Implement adding individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -40,7 +40,45 @@ function deleteSearchHistoryItem(e) {
   }
 }
 
+function addSearchHistory() {
+  const input = this.value.trim()
+  if (input.length) {
+    const listItem = createSearchHistoryItem(input)
+    gnbSearchHistoryList.append(listItem)
+  }
+  this.value = ''
+
+  openGnbSearchHistory()
+}
+
+function createSearchHistoryItem(name) {
+  const listItem = document.createElement('li')
+  listItem.classList.add('search-history-item')
+
+  const wordButton = document.createElement('button')
+  wordButton.classList.add('word-button')
+  wordButton.setAttribute('type', 'button')
+  listItem.append(wordButton)
+
+  const searchText = document.createTextNode(name)
+  wordButton.append(searchText)
+
+  const deleteButton = document.createElement('button')
+  deleteButton.classList.add('delete-button')
+  deleteButton.setAttribute('type', 'button')
+  deleteButton.addEventListener('click', deleteSearchHistoryItem)
+  listItem.append(deleteButton)
+
+  const deleteIcon = document.createElement('i')
+  deleteIcon.classList.add('ic-close')
+  deleteIcon.setAttribute('aria-label', '검색어 삭제')
+  deleteButton.append(deleteIcon)
+
+  return listItem
+}
+
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+gnbSearchInput.addEventListener('change', addSearchHistory)
 deleteAllButton.addEventListener('click', deleteAllSearchHistory)
 deleteButtonList.forEach((button) => {
   button.addEventListener('click', deleteSearchHistoryItem)
